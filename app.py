57f283e6988e3a3619a7e748965f2a461704a515
Ubuntu <ubuntu@ip-172-31-18-12.us-east-2.compute.internal>
2020-12-22 08:01:03 +0000

    week4 day2 commit

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from model import MLModelHandler, DLModelHandler
 app = Flask(__name__)
 
 # assign model handler as global variable [2 LINES]
+ml_handler = MLModelHandler()
+dl_handler = DLModelHandler()
 
 
 @app.route("/predict", methods=["POST"])
@@ -16,9 +18,9 @@ def predict():
 
     # model inference [2 LINES]
     if do_fast:
-        pass
+        predictions = ml_handler.handle(text)
     else:
-        pass
+        predictions = dl_handler.handle(text)
 
     # response
     result = json.dumps({str(i): {'text': t, 'label': l, 'confidence': c}
--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import torch
 import itertools
 from utils import clean_text
 
@@ -26,30 +27,35 @@ class MLModelHandler(ModelHandler):
 
     def initialize(self, ):
         # De-serializing model and loading vectorizer
-        
-        pass
+        import joblib
+        self.model = joblib.load('model/ml_model.pkl')
+        self.vectorizer = joblib.load('model/ml_vectorizer.pkl')
 
-    def preprocess(self, data):
+    def preprocess(self, text):
         # cleansing raw text
-
-        # vectorizing cleaned text
-
-        return data
-
-    def inference(self, data):
-        # get predictions from model as probabilities
+        model_input = self._clean_text(text)
         
-        return data
+        # vectorizing cleaned text
+        model_input = self.vectorizer.transform(model_input)
+        return model_input
 
-    def postprocess(self, data):
+    def inference(self, model_input):
+        # get predictions from model as probabilities
+        model_output = self.model.predict_proba(model_input)
+        return model_output
+
+    def postprocess(self, model_output):
         # process predictions to predicted label and output format
-
-        return data
+        predicted_probabilities = model_output.max(axis=1)
+        predicted_ids = model_output.argmax(axis=1)
+        predicted_labels = [self.id2label[id_] for id_ in predicted_ids]
+        return predicted_labels, predicted_probabilities
 
     def handle(self, data):
         # do above processes
-
-        return data
+        model_input = self.preprocess(data)
+        model_output = self.inference(model_input)
+        return self.postprocess(model_output)
 
 
 class DLModelHandler(ModelHandler):
@@ -58,16 +64,33 @@ class DLModelHandler(ModelHandler):
         self.initialize()
 
     def initialize(self, ):
-        pass
+        from transformers import AutoTokenizer, AutoModelForSequenceClassification
+        self.model_name_or_path = 'sackoh/bert-base-multilingual-cased-nsmc'
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name_or_path)
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.model_name_or_path)
+        self.model.to('cpu')
 
-    def preprocess(self, data):
-        return data
+    def preprocess(self, text):
+        model_input = self.clean_text(text)
+        model_input = self.tokenizer(text, return_tensors='pt', padding=True)
+        return model_input
 
-    def inference(self, data):
-        return data
+    def inference(self, model_input):
+        with torch.no_grad():
+            model_output = self.model(**model_input)[0].cpu()
+            model_output = 1.0 / (1.0 + torch.exp(-model_output))
+            model_output = model_output.numpy().astype('float')
+        return model_output
 
-    def postprocess(self, data):
-        return data
+    def postprocess(self, model_output):
+        # process predictions to predicted label and output format
+        predicted_probabilities = model_output.max(axis=1)
+        predicted_ids = model_output.argmax(axis=1)
+        predicted_labels = [self.id2label[id_] for id_ in predicted_ids]
+        return predicted_labels, predicted_probabilities
 
     def handle(self, data):
-        return data
+        # do above processes
+        model_input = self.preprocess(data)
+        model_output = self.inference(model_input)
+        return self.postprocess(model_output)
